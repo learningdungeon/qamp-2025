2852a58a1006cc60e40aa3e1d8a03fb58d39ded1
Noor <200412177+learningdungeon@users.noreply.github.com>
2025-10-21 01:11:00 +0000

    Cleaned repo: removed unnecessary folders to match official structure

--- Points/Untitled-1.bat
+++
@@ -1,6 +0,0 @@
-echo "# Curriculum Folder" > curriculum/README.md
-echo "# Labs Folder" > labs/README.md
-echo "# Assessments Folder" > assessments/README.md
-echo "# Translations Folder" > translations/README.md
-echo "# Docs Folder" > docs/README.md
-echo "# Blog Folder" > blog/README.md
